795035d6ef3a65239626d81eebbb4bc8faa8431f
ash9river <ash9river@gmail.com>
2024-12-13 11:44:40 +0900

    feat: msw 라이브러리 도입

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,30 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import { QueryClientProvider } from '@tanstack/react-query';
+import queryClient from 'services/tanstackQueryClient.ts';
 
-createRoot(document.getElementById('root')!).render(
+/* createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-);
+); */
+
+async function deferRender() {
+  if (process.env.NODE_ENV !== 'development') {
+    return;
+  }
+  const { worker } = await import('./mocks/browser');
+
+  return worker.start();
+}
+
+deferRender().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </StrictMode>,
+  );
+});
