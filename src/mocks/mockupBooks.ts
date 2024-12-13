7e711a31d60ae951c4c50537ad6ae54e5f87e59c
ash9river <ash9river@gmail.com>
2024-12-13 11:47:33 +0900

    feat: mockup 데이터 추가

---
+++ src/mocks/mockupBooks.ts
@@ -0,0 +1,92 @@
+import { Book } from 'types/Book';
+
+export const mockupBooks: Book[] = [
+  {
+    bookId: 1,
+    title: '소년이 온다1',
+    author: '한강1',
+    price: 15000,
+    stock: 11,
+  },
+  {
+    bookId: 2,
+    title: '작별하지 않는다2',
+    author: '한강2',
+    price: 15120,
+    stock: 12,
+  },
+  {
+    bookId: 3,
+    title: '채식주의자3',
+    author: '한강3',
+    price: 15000,
+    stock: 13,
+  },
+  {
+    bookId: 4,
+    title: '소년이 온다4',
+    author: '한강4',
+    price: 15000,
+    stock: 11,
+  },
+  {
+    bookId: 5,
+    title: '작별하지 않는다5',
+    author: '한강5',
+    price: 15120,
+    stock: 12,
+  },
+  {
+    bookId: 6,
+    title: '채식주의자6',
+    author: '한강6',
+    price: 15000,
+    stock: 13,
+  },
+  {
+    bookId: 7,
+    title: '소년이 온다7',
+    author: '한강7',
+    price: 15000,
+    stock: 11,
+  },
+  {
+    bookId: 8,
+    title: '작별하지 않는다8',
+    author: '한강8',
+    price: 15120,
+    stock: 12,
+  },
+  {
+    bookId: 9,
+    title: '채식주의자9',
+    author: '한강9',
+    price: 15000,
+    stock: 13,
+  },
+  {
+    bookId: 10,
+    title: '소년이 온다10',
+    author: '한강10',
+    price: 15000,
+    stock: 11,
+  },
+  {
+    bookId: 11,
+    title: '작별하지 않는다11',
+    author: '한강11',
+    price: 15120,
+    stock: 12,
+  },
+  {
+    bookId: 12,
+    title: '채식주의자12',
+    author: '한강12',
+    price: 15000,
+    stock: 13,
+  },
+];
+
+export const mockupBooksCounter = {
+  mockupBooksCounter: mockupBooks.length + 1,
+};
