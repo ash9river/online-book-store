59331c30460263c3b108319c9e5834e68b7edc11
ash9river <ash9river@gmail.com>
2024-12-14 14:34:21 +0900

    feat: 리액트쿼리프로바이더 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { QueryClientProvider } from '@tanstack/react-query';
-import queryClient from 'services/tanstackQueryClient.ts';
+import queryClient from './services/tanstackQueryClient.ts';
+import './index.css';
 
 /* createRoot(document.getElementById('root')!).render(
   <StrictMode>
